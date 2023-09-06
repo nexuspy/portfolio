a4d57fc0f1f3b47fee8cc21870e3a99ecd72e9a1
nexuspy <59784880+nexuspy@users.noreply.github.com>
2023-09-06 23:26:35 +0530

    changes in work

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -33,8 +33,8 @@ const description =
 const skillsList = [
   "Web design",
   "User experience",
-  "User design",
-  "Focus group testing",
+  "User design
+  "Focus group t
   "Mobile user interfaces",
   "Graphic design",
 ];
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible to all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.  By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible to all.";
 
 const About = () => {
   return (
